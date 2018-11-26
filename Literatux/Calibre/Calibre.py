a7708cd9fd119f37786cab9df120d68e7f2401dc
gdripoll <gdripoll@gmail.com>
2018-11-26 20:13:38 -0300

    develop

--- Literatux/Calibre/Calibre.py
+++ Literatux/Calibre/Calibre.py
@@ -7,47 +7,69 @@ import logging.config
 
 
 class Calibre:
-    def __init__(self, config):
-        log = '<Calibre:__init__> %s'
-        self._config = config
-        self._conn = None
-        logging.debug(log, 'init')
-        self._open()
+	def __init__(self, config):
+		log = '<Calibre:__init__> %s'
+		self._config = config
+		self._conn = None
+		logging.debug(log, 'init')
+		self._open()
 
-    def _open(self):
-        log = '<Calibre:__init__> %s'
-        if self._conn is not None:
-            self._close()
-        if 'path' in self._config.keys():
-            try:
-                self._conn = sqlite3.connect('%s/metadata.db' % self._config['path'])
-                self._db = self._conn.cursor()
-                logging.debug(log, 'connected to %s' % self._config['name'])
-            except Exception as err:
-                logging.error("cannot connect to %s > %s" % ('%s/metadata.db' % self._config['path'], err))
+	def _open(self):
+		log = '<Calibre:__init__> %s'
+		if self._conn is not None:
+			self._close()
+		if 'path' in self._config.keys():
+			try:
+				self._conn = sqlite3.connect('%s/metadata.db' % self._config['path'])
+				self._db = self._conn.cursor()
+				logging.debug(log, 'connected to %s' % self._config['name'])
+			except Exception as err:
+				logging.error("cannot connect to %s > %s" % ('%s/metadata.db' % self._config['path'], err))
 
-    def _close(self):
-        if self._conn is not None:
-            try:
-                self._conn.close()
-            except Exception as err:
-                logging.error("cannot disconnect from %s > %s" % (self._config.path, err))
+	def _close(self):
+		if self._conn is not None:
+			try:
+				self._conn.close()
+			except Exception as err:
+				logging.error("cannot disconnect from %s > %s" % (self._config.path, err))
 
-    def read_data_books(self):
-        log = '<Calibre:read_data_books> %s'
-        try:
-            self._db.execute("""select * from books order by sort""")
-            sal = []
-            for row in self._db:
-                sal.append(self.dict_factory(self._db, row))
-            logging.debug(log, 'read %s rows.' % len(sal))
-            return sal
-        except Exception as err:
-            logging.error("cannot read from %s > %s" % (self._config, err))
+	def read_data_books(self):
+		log = '<Calibre:read_data_books> %s'
+		try:
+			logging.debug(log, 'reading %s.' % self._config['name'])
+			cmd = """
+			SELECT * from books_authors_link as ba
+			LEFT JOIN books b on ba.book = b.id
+			LEFT JOIN data d on ba.book = d.book 
+			order by sort, format
+			"""
+			self._db.execute(cmd)
+			sal = {}
+			for row in self._db:
+				r = self.dict_factory(self._db, row)
+				if r['book'] in sal:
+					sal[r['book']]['formats'].append(r['format'])
+				else:
+					sal[r['book']] = {
+						"collection": self._config['name'],
+						"book_id"   : r['book'],
+						"auth_id"   : r['author'],
+						"title"     : r['sort'],
+						"author"    : r['author_sort'],
+						"path"      : r['path'],
+						"name"      : r['name'],
+						"formats"   : [r['format']]
+					}
+			logging.debug(log, 'read %s rows.' % len(sal))
+			# for s in sal:
+			# 	print(s, sal[s])
+			return sal
+		except Exception as err:
+			logging.error("cannot read from %s > %s" % (self._config, err))
 
-    @staticmethod
-    def dict_factory(cursor, row):
-        d = {}
-        for idx, col in enumerate(cursor.description):
-            d[col[0]] = row[idx]
-        return d
+	@staticmethod
+	def dict_factory(cursor, row):
+		d = {}
+		for idx, col in enumerate(cursor.description):
+			d[col[0]] = row[idx]
+		return d
--- Literatux/MySql/MySql.py
+++ Literatux/MySql/MySql.py
@@ -9,83 +9,92 @@ import logging.config
 
 
 class MySql:
-    def __init__(self, name, config):
-        log = '<MySql:__init__> %s'
-        self._config = config
-        self._name = name
-        self._conn = None
-        self._cursor = None
-        logging.debug(log, 'init')
-        self._open()
+	def __init__(self, name, config):
+		log = '<MySql:__init__> %s'
+		self._config = config
+		self._name = name
+		self._conn = None
+		self._cursor = None
+		logging.debug(log, 'init')
+		self._open()
 
-    def _open(self):
-        log = '<MySql:_open> %s'
-        if self._conn is not None:
-            self._close()
-        try:
-            self._conn = pymysql.connect(self._config['host'], self._config['user'], self._config['password'], self._config['database'])
-            self._cursor = self._conn.cursor()
-            logging.debug(log, 'connected to %s [%s] ' % (self._name, self._config['database']))
-        except Exception as err:
-            logging.error(log, "cannot connect to %s > %s" % ('%s/metadata.db' % self._config['path'], err))
+	def _open(self):
+		log = '<MySql:_open> %s'
+		if self._conn is not None:
+			self._close()
+		try:
+			self._conn = pymysql.connect(self._config['host'], self._config['user'], self._config['password'], self._config['database'])
+			self._cursor = self._conn.cursor()
+			logging.debug(log, 'connected to %s [%s] ' % (self._name, self._config['database']))
+		except Exception as err:
+			logging.error(log, "cannot connect to %s > %s" % ('%s/metadata.db' % self._config['path'], err))
 
-    def _close(self):
-        log = '<MySql:_close> %s'
-        if self._conn is not None:
-            try:
-                self._conn.close()
-            except Exception as err:
-                logging.error(log, "cannot disconnect from %s > %s" % (self._config['path'], err))
+	def _close(self):
+		log = '<MySql:_close> %s'
+		if self._conn is not None:
+			try:
+				self._conn.close()
+			except Exception as err:
+				logging.error(log, "cannot disconnect from %s > %s" % (self._config['path'], err))
 
-    def drop_table(self):
-        log = '<MySql:drop_table> %s'
-        try:
-            cmd = """
+	def drop_table(self):
+		log = '<MySql:drop_table> %s'
+		try:
+			cmd = """
                 DROP TABLE IF EXISTS {table};
             """.format(
-                table=self._name
-            )
-            logging.debug(log, cmd)
-            self._cursor.execute(cmd)
-        except Exception as err:
-            logging.error(log, err)
+				table=self._name
+			)
+			logging.debug(log, cmd)
+			self._cursor.execute(cmd)
+		except Exception as err:
+			logging.error(log, err)
 
-    def create(self):
-        log = '<MySql:create> %s'
-        try:
-            self.drop_table()
-            cmd = """
-                CREATE TABLE `calibre2mysql`.`{table}` ( 
-                    `cal_id` BIGINT NOT NULL AUTO_INCREMENT , 
-                    `cal_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-                    `cal_book_name` VARCHAR(500) NOT NULL , 
-                    `cal_author_name` VARCHAR(500) NOT NULL , 
-                    PRIMARY KEY (`cal_id`)
-                ) ENGINE = MyISAM;
-                """.format(
-                table=self._name
-            )
-            logging.debug(log, cmd)
-            self._cursor.execute(cmd)
-        except Exception as err:
-            logging.error(log, err)
+	def create(self):
+		log = '<MySql:create> %s'
+		try:
+			self.drop_table()
+			cmd = """
+				CREATE TABLE `calibre2mysql`.`{table}` ( 
+					`cal_id` BIGINT NOT NULL AUTO_INCREMENT , 
+					`cal_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+					`cal_collection` VARCHAR(20) NOT NULL,
+					`cal_book_id` INTEGER NOT NULL,
+					`cal_auth_id` INTEGER NOT NULL,
+					`cal_book_name` VARCHAR(500) NOT NULL , 
+					`cal_author_name` VARCHAR(500) NOT NULL ,
+					`cal_path` VARCHAR(500) NOT NULL,
+					`cal_name` VARCHAR(500) NOT NULL,
+					`cal_formats` VARCHAR(100) NOT NULL,
+					PRIMARY KEY (`cal_id`)
+				) ENGINE = MyISAM;
+			""".format(table=self._name)
+			logging.debug(log, cmd)
+			self._cursor.execute(cmd)
+		except Exception as err:
+			logging.error(log, err)
 
-    def save_rows(self, rows):
-        log = '<MySql:save_rows> %s'
+	def save_rows(self, rows):
+		log = '<MySql:save_rows> %s'
 
-        logging.debug(log, "creating %s rows." % len(rows))
-        cmd = """
-            INSERT INTO `{table}` 
-                (`cal_book_name`, `cal_author_name`) 
-            VALUES 
-                (%s, %s)
-        """.format(
-            table=self._name
-        )
-        data = []
-        for r in rows:
-            data.append((r['sort'], r['author_sort']))
-
-        logging.debug(log, "saving %s rows." % len(rows))
-        self._cursor.executemany(cmd, data)
-        logging.debug(log, "%s rows saved." % len(rows))
+		logging.debug(log, "creating %s rows." % len(rows))
+		cmd = """
+			INSERT INTO `{table}` 
+			(`cal_collection`,`cal_book_id`,`cal_auth_id`,`cal_book_name`,`cal_author_name`,`cal_path`,`cal_name`,`cal_formats`) 
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+			""".format(table=self._name)
+		data = []
+		for key, r in rows.items():
+			data.append((
+				r['collection'],
+				r['book_id'],
+				r['auth_id'],
+				r['title'],
+				r['author'],
+				r['path'],
+				r['name'],
+				",".join(r['formats'])
+			))
+		logging.debug(log, "saving %s rows." % len(rows))
+		self._cursor.executemany(cmd, data)
+		logging.debug(log, "%s rows saved." % len(rows))
